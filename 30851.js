f1957e6571f8567a03c54e9a88d78bd9677b032a
RomanFisc <romanfisc@gmail.com>
2024-05-28 16:07:40 -0400

    part 2 finished

--- 30851.js
+++ 30851.js
@@ -86,3 +86,35 @@ function isLess() {
 }
 
 isLess()
+
+
+// Part 2
+let array = [
+  { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+  { id: "48", name: "Barry", occupation: "Runner", age: "25" },
+  { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+  { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+  { id: "7", name: "Bilbo", occupation: "None", age: "111" }
+]
+
+// Use callback functions alongside Array methods to accomplish the following:
+// Sort the array by age.
+// Filter the array to remove entries with an age greater than 50.
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+// Use the reduce method to calculate the sum of the ages.
+// Then use the result to calculate the average age.
+
+array.sort((a,b) =>  a.age - b.age);
+array = array.filter(a => a.age < 50);
+
+
+let sumAge = array.reduce((sum, entry) => sum + parseInt(entry.age), 0)
+let avgAge = sumAge / array.length
+
+
+console.log(array)
+console.log(sumAge)
+console.log(avgAge)
+
+
+/Part 2
